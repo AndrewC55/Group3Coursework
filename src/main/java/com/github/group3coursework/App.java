7f2b12cc0cd7e084b969da2faee0c6692ffeb7f0
Wojciech Smigielski <40417348@live.napier.ac.uk>
2020-01-21 22:39:24 +0000

    Created App.java and included code to test it runs correctly.

---
+++ src/main/java/com/github/group3coursework/App.java
@@ -0,0 +1,8 @@
+package com.github.group3coursework;
+
+public class App {
+    public static void main(String[] args)
+    {
+        System.out.println("Project builds to self contained JAR with Maven");
+    }
+}
