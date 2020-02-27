abb2ef41bd32cb55fc3592cc3d7ad9d1c1de3235
Andrew Clark <andrew.clark@solarwinds.com>
2020-02-27 11:36:45 +0000

    Changed variables to private and made getters and setters

--- src/main/java/com/github/group3coursework/Capital.java
+++ src/main/java/com/github/group3coursework/Capital.java
@@ -2,9 +2,66 @@ package com.github.group3coursework;
 
 class Capital {
 
-  String name;
+  /**
+   * Capital's name
+   */
+  private String name;
 
-  int population;
+  /**
+   * Capital's population
+   */
+  private int population;
 
-  String country;
+  /**
+   * Capital's country
+   */
+  private String country;
+
+  /**
+   * Getter function for name
+   * @return String name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Setter function for name
+   * @param name capital's name
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * Getter function for population
+   * @return int population
+   */
+  public int getPopulation() {
+    return population;
+  }
+
+  /**
+   * Setter function for population
+   * @param population capital's population
+   */
+  public void setPopulation(int population) {
+    this.population = population;
+  }
+
+  /**
+   * Getter function for country
+   * @return String country
+   */
+  public String getCountry() {
+    return country;
+  }
+
+  /**
+   * Setter function for country
+   * @param country capital's country
+   */
+  public void setCountry(String country) {
+    this.country = country;
+  }
 }
--- src/main/java/com/github/group3coursework/Country.java
+++ src/main/java/com/github/group3coursework/Country.java
@@ -2,13 +2,108 @@ package com.github.group3coursework;
 
 class Country {
 
-  String name;
+  /**
+   * Country's name
+   */
+  private String name;
 
-  String continent;
+  /**
+   * Country's continent
+   */
+  private String continent;
 
-  String region;
+  /**
+   * Country's region
+   */
+  private String region;
 
-  int population;
+  /**
+   * Country's population
+   */
+  private int population;
 
-  String capitalCity;
+  /**
+   * Country's capital city
+   */
+  private String capitalCity;
+
+  /**
+   * Getter function for name
+   * @return String name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Setter function for name
+   * @param name country's name
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * Getter function for continent
+   * @return String continent
+   */
+  public String getContinent() {
+    return continent;
+  }
+
+  /**
+   * Setter function for continent
+   * @param continent country's continent
+   */
+  public void setContinent(String continent) {
+    this.continent = continent;
+  }
+
+  /**
+   * Getter function for region
+   * @return String region
+   */
+  public String getRegion() {
+    return region;
+  }
+
+  /**
+   * Setter function for region
+   * @param region country's region
+   */
+  public void setRegion(String region) {
+    this.region = region;
+  }
+
+  /**
+   * Getter function for population
+   * @return int population
+   */
+  public int getPopulation() {
+    return population;
+  }
+
+  /**
+   * Setter function for population
+   * @param population country's population
+   */
+  public void setPopulation(int population) {
+    this.population = population;
+  }
+
+  /**
+   * Getter function for capital city
+   * @return String capitalCity
+   */
+  public String getCapitalCity() {
+    return capitalCity;
+  }
+
+  /**
+   * Setter function for capital city
+   * @param capitalCity country's capital city
+   */
+  public void setCapitalCity(String capitalCity) {
+    this.capitalCity = capitalCity;
+  }
 }
