051b06ceef30363f0b09438368253096035a9546
Andrew Clark <andrew.clark@solarwinds.com>
2020-03-05 11:22:37 +0000

    Updated github issues comment in Populationhandler

--- src/main/java/com/github/group3coursework/Population/PopulationHandler.java
+++ src/main/java/com/github/group3coursework/Population/PopulationHandler.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 /**
  * This is a handler that the main will call
  * This is used to determine which population query the user would like to view
- * This will be used for github issues 26 - 32
+ * This will be used for github issues 32 - 37
  */
 public class PopulationHandler {
 
